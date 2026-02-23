8637bf76a0da5d9b9a9f47c4315cf79d78fd2ba5
Vishal Pambla <v-vpambla@abarcahealth.com>
2026-02-23 11:25:59 -0400

    deploying all code folders/files

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,5 +1,10 @@
 variable "vpc_cidr" {}
-variable "vpc_name" {}
+
 variable "public_subnets" { type = list(string) }
 variable "private_subnets" { type = list(string) }
 variable "availability_zones" { type = list(string) }
+
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
